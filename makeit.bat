a7df06525189e3bf4eb0f54465a02669a2242256
DarwinWasWrong <leibhold@ozemail.com.au>
2026-02-25 16:47:32 +0930

    batch file

--- makeit.bat
+++ makeit.bat
@@ -1,6 +1,9 @@
 v:
 cd V:\wokwi-projects\wokwi-LN298module-chip
 wokwi-cli chip compile src\chip-moduleLN298.chip.c
-copy V:\wokwi-projects\wokwi-LN298module-chip\src\chip-moduleLN298.chip.wasm V:\wokwi-projects\wokwi-LN298module-chip\dist\chip.wasm 
-copy V:\wokwi-projects\wokwi-LN298module-chip\src\chip-moduleLN298.chip.json V:\wokwi-projects\wokwi-LN298module-chip\dist\chip.json
- tar.exe -a -c -f V:\wokwi-projects\wokwi-LN298module-chip\dist\chip.zip V:\wokwi-projects\wokwi-LN298module-chip\src\chip-moduleLN298.chip.json V:\wokwi-projects\wokwi-LN298module-chip\src\chip-moduleLN298.chip.wasm V:\wokwi-projects\wokwi-LN298module-chip\dist\chip.wasm 
+cd dist
+copy ..\src\chip-moduleLN298.chip.wasm chip.wasm 
+copy ..\src\chip-moduleLN298.chip.json chip.json
+
+tar.exe -acf chip.zip chip.json chip.wasm
+cd ..
